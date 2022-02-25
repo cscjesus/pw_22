88fc1491cea0620e9a70901204487f5e86edba7e
Jesus HL <csc.jesus@gmail.com>
2022-02-25 10:28:04 -0600

    sesion1.js terminado

--- javascript/sesion1.js
+++ javascript/sesion1.js
@@ -1,13 +1,25 @@
 function saludar(){
   var nombre = document.getElementById('txtNombre').value;
   var apellido = document.getElementById('txtApellido').value;
+  
   alert("Bienvenido "+nombre+" "+apellido);
+  var color = document.getElementById('miColor').value;
+  var lienzo = document.getElementById('miDiv');
+  lienzo.style.background=color;
   //console.log("Bienvenido "+nombre+" "+apellido);
   //alert('Hola mundo');
 }
 function cambiarColor(){
-  var color = document.getElementById('miColor').value;
+  /*var color = document.getElementById('miColor').value;
   var lienzo = document.getElementById('miDiv');
-  lienzo.style.background=color;
+  lienzo.style.background=color;*/
+
   //alert('hola color');
+}
+function crearDegradado(){
+  //alert("ok");
+  var color1 = document.getElementById("color1").value;
+  var color2 = document.getElementById("color2").value;
+  var lienzo = document.getElementById("degradado");
+  lienzo.style.background = "linear-gradient("+color1+","+color2+")";
 }
