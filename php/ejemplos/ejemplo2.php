e2f7df7e0a1ebaad20a3843340f8d552c4b3733c
Jesus H L <csc.jesus@gmail.com>
2022-04-29 11:08:43 -0500

    ejemplo3 terminado

---
+++ php/ejemplos/ejemplo2.php
@@ -0,0 +1,29 @@
+<?php
+//funciones
+function saludo()
+{
+    echo 'Hola mundo';
+}
+function suma($a, $b)
+{
+    return $a + $b;
+}
+function producto($a, $b)
+{
+    return $a * $b;
+}
+//ejemplo2.php?num1=2&num2=6&op=s
+$a = $_REQUEST['num1'];
+$b = $_REQUEST['num2'];
+$op = $_REQUEST['op'];
+switch ($op) {
+    case 's':
+        echo 'La suma es: ' . suma($a, $b);
+        break;
+    case 'p':
+        echo 'El producto es: ' . producto($a, $b);
+        break;
+    default:
+        echo 'Operacion no valida';
+}
+// saludo();
---
+++ php/ejemplos/ejemplo3.php
@@ -0,0 +1,76 @@
+<?php
+// ext install phproberto.vscode-php-getters-setters
+class Person{
+    private $nombre;
+    private $apellido;
+    private $edad;
+    
+    function __construct($nombre,$apellido,$edad)
+    {
+        $this->nombre;
+        $this->apellido;
+        $this->edad;
+    }
+    function mostrar(){
+        return get_object_vars()
+    }
+    /**
+     * Get the value of nombre
+     */ 
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @return  self
+     */ 
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of apellido
+     */ 
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    /**
+     * Set the value of apellido
+     *
+     * @return  self
+     */ 
+    public function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of edad
+     */ 
+    public function getEdad()
+    {
+        return $this->edad;
+    }
+
+    /**
+     * Set the value of edad
+     *
+     * @return  self
+     */ 
+    public function setEdad($edad)
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+}
